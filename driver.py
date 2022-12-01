c87ea2c2c2266c8a9fbe9cd6508515834f0280ad
Daniel Gonzalez <danielgonzalez3@csus.edu>
2022-11-30 19:41:19 -0800

    rm jupyter + driver clean up

--- driver.py
+++ driver.py
@@ -9,77 +9,37 @@ from tensorflow.keras import layers
 from datasets import load_dataset
 from transformers import TFWav2Vec2Model, AutoFeatureExtractor
 
+BATCH = 32         # batch size for training and evaluating our model.
+NUM_CLASSES = 10   # number of classes our dataset will have (11 in our case).
+HIDDEN_DIM = 768   # dim of our model output (768 in our case)
+SEQ_LENGTH = 16000 # length of audio
+MAX_FRAMES = 49    # wav2Vec 2.0 results
+
+MODEL_CHECKPOINT = "facebook/wav2vec2-base"  # pretrained model
+
 tf.get_logger().setLevel(logging.ERROR)
 tf.keras.utils.set_random_seed(42)
 
-# Maximum duration of the input audio file we feed to our Wav2Vec 2.0 model.
-MAX_DURATION = 1
-
-SAMPLING_RATE = 16000 # Sampling rate is the number of samples of audio recorded every second
-BATCH_SIZE = 32  # Batch-size for training and evaluating our model.
-NUM_CLASSES = 10  # Number of classes our dataset will have (11 in our case).
-HIDDEN_DIM = 768  # Dimension of our model output (768 in case of Wav2Vec 2.0 - Base).
-MAX_SEQ_LENGTH = MAX_DURATION * SAMPLING_RATE  # Maximum length of the input audio file.
-
-# Wav2Vec 2.0 results in an output frequency with a stride of about 20ms.
-MAX_FRAMES = 49
-MAX_EPOCHS = 2  # Maximum number of training epochs.
-MODEL_CHECKPOINT = "facebook/wav2vec2-base"  # Name of pretrained model from Hugging Face Model Hub
-
-speech_commands_v1 = load_dataset("superb", "ks", ignore_verifications=True)
-
-print("SPEECH CMDS:\n", speech_commands_v1)
-
-speech_commands_v1 = speech_commands_v1["train"].train_test_split(
-    train_size=0.5, test_size=0.5, stratify_by_column="label"
-)
-
-speech_commands_v1 = speech_commands_v1.filter(
-    lambda x: x["label"]
-    != (
-        speech_commands_v1["train"].features["label"].names.index("_unknown_")
-        and speech_commands_v1["train"].features["label"].names.index("_silence_")
+def preprocess_function(data):
+    # A feature extractor is in charge of preparing input features for audio or vision models. 
+    # This includes feature extraction from sequences, e.g., pre-processing audio files to Log-Mel Spectrogram features
+    feature_extractor = AutoFeatureExtractor.from_pretrained(
+        MODEL_CHECKPOINT, return_attention_mask=True
     )
-)
-
-speech_commands_v1["train"] = speech_commands_v1["train"].select(
-    [i for i in range((len(speech_commands_v1["train"]) // BATCH_SIZE) * BATCH_SIZE)]
-)
-speech_commands_v1["test"] = speech_commands_v1["test"].select(
-    [i for i in range((len(speech_commands_v1["test"]) // BATCH_SIZE) * BATCH_SIZE)]
-)
-
-print("SPEECH CMDS:\n", speech_commands_v1)
-
-
-labels = speech_commands_v1["train"].features["label"].names
-
-feature_extractor = AutoFeatureExtractor.from_pretrained(
-    MODEL_CHECKPOINT, return_attention_mask=True
-)
-
-
-def preprocess_function(examples):
-    audio_arrays = [x["array"] for x in examples["audio"]]
+    audio_arrays = [x["array"] for x in data["audio"]]
     inputs = feature_extractor(
+        # audio input
         audio_arrays,
+        # the sampling rate at which the audio files should be digitalized expressed in Hertz per second.
         sampling_rate=feature_extractor.sampling_rate,
-        max_length=MAX_SEQ_LENGTH,
+        # maximum length of the returned list and optionally padding length
+        max_length=SEQ_LENGTH,
+        # activates truncation to cut input sequences longer than max_length to max_length.
         truncation=True,
+        # pad to the longest sequence in the batch (or no padding if only a single sequence if provided).
         padding=True,
     )
     return inputs
-
-
-# This line with pre-process our speech_commands_v1 dataset. We also remove the "audio"
-# and "file" columns as they will be of no use to us while training.
-processed_speech_commands_v1 = speech_commands_v1.map(
-    preprocess_function, remove_columns=["audio", "file"], batched=True
-)
-
-# Load the whole dataset splits as a dict of numpy arrays
-train = processed_speech_commands_v1["train"].shuffle(seed=42).with_format("numpy")[:]
-test = processed_speech_commands_v1["test"].shuffle(seed=42).with_format("numpy")[:]
 
 def mean_pool(hidden_states, feature_lengths):
     attenion_mask = tf.sequence_mask(
@@ -91,7 +51,7 @@ def mean_pool(hidden_states, feature_lengths):
     )
     hidden_states = tf.where(
         tf.broadcast_to(
-            tf.expand_dims(~padding_mask, -1), (BATCH_SIZE, MAX_FRAMES, HIDDEN_DIM)
+            tf.expand_dims(~padding_mask, -1), (BATCH, MAX_FRAMES, HIDDEN_DIM)
         ),
         0.0,
         hidden_states,
@@ -102,39 +62,37 @@ def mean_pool(hidden_states, feature_lengths):
     )
     return pooled_state
 
-
+# combines encoder/decoder for an E2E model
 class TFWav2Vec2ForAudioClassification(layers.Layer):
-    """Combines the encoder and decoder into an end-to-end model for training."""
-
     def __init__(self, model_checkpoint, num_classes):
         super(TFWav2Vec2ForAudioClassification, self).__init__()
-        # Instantiate the Wav2Vec 2.0 model without the Classification-Head
+        # init wav2vec 2.0 model without the classification head
         self.wav2vec2 = TFWav2Vec2Model.from_pretrained(
-            model_checkpoint, apply_spec_augment=False, from_pt=True
+            model_checkpoint, 
+            apply_spec_augment=False, 
+            from_pt=True
         )
         self.pooling = layers.GlobalAveragePooling1D()
-        # Drop-out layer before the final Classification-Head
+        # drop-out layer before the final classification head
         self.intermediate_layer_dropout = layers.Dropout(0.5)
-        # Classification-Head
-        self.final_layer = layers.Dense(num_classes+2, activation="softmax")
+        # classification head with softmax activation
+        self.final_layer = layers.Dense(num_classes, activation="softmax")
 
     def call(self, inputs):
-        # We take only the first output in the returned dictionary corresponding to the
-        # output of the last layer of Wav2vec 2.0
+        # use first output in the returned dictionary corresponding to output of the last layer
         hidden_states = self.wav2vec2(inputs["input_values"])[0]
 
-        # If attention mask does exist then mean-pool only un-masked output frames
+        # if attention mask does exist then mean-pool only un-masked output frames
         if tf.is_tensor(inputs["attention_mask"]):
-            # Get the length of each audio input by summing up the attention_mask
-            # (attention_mask = (BATCH_SIZE x MAX_SEQ_LENGTH) ∈ {1,0})
+            # length of each audio input by summing up the attention_mask
+            # (attention_mask = (BATCH x SEQ_LENGTH) ∈ {1,0})
             audio_lengths = tf.cumsum(inputs["attention_mask"], -1)[:, -1]
-            # Get the number of Wav2Vec 2.0 output frames for each corresponding audio input
-            # length
+            # retrive numbe of wav2Vec output frams for each inputted audio length
             feature_lengths = self.wav2vec2.wav2vec2._get_feat_extract_output_lengths(
                 audio_lengths
             )
             pooled_state = mean_pool(hidden_states, feature_lengths)
-        # If attention mask does not exist then mean-pool only all output frames
+        # if attention mask does not exist then mean-pool only all output frames
         else:
             pooled_state = self.pooling(hidden_states)
 
@@ -144,43 +102,67 @@ class TFWav2Vec2ForAudioClassification(layers.Layer):
         return final_state
 
 def build_model():
-    # Model's input
     inputs = {
-        "input_values": tf.keras.Input(shape=(MAX_SEQ_LENGTH,), dtype="float32"),
-        "attention_mask": tf.keras.Input(shape=(MAX_SEQ_LENGTH,), dtype="int32"),
+        "input_values": tf.keras.Input(shape=(SEQ_LENGTH,), dtype="float32"),
+        "attention_mask": tf.keras.Input(shape=(SEQ_LENGTH,), dtype="int32"),
     }
-    # Instantiate the Wav2Vec 2.0 model with Classification-Head using the desired
-    # pre-trained checkpoint
-    wav2vec2_model = TFWav2Vec2ForAudioClassification(MODEL_CHECKPOINT, NUM_CLASSES)(
-        inputs
-    )
-    # Model
+    # init wav2vec from facebook, include filtered classes
+    wav2vec2_model = TFWav2Vec2ForAudioClassification(MODEL_CHECKPOINT, NUM_CLASSES+2)(inputs)
+
     model = tf.keras.Model(inputs, wav2vec2_model)
-    # Loss
     loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
-    # Optimizer
     optimizer = keras.optimizers.Adam(learning_rate=1e-5)
-    # Compile and return
     model.compile(loss=loss, optimizer=optimizer, metrics=["accuracy"])
     return model
 
-model = build_model()
+def main():
+    # load model, ignore checksum
+    init_dataset = load_dataset("superb", "ks", ignore_verifications=True)
+    print("init dataset:\n", init_dataset)
 
-train_x = {x: y for x, y in train.items() if x != "label"}
-test_x = {x: y for x, y in test.items() if x != "label"}
+    # filter 1/2 from existing training set for both train and test data
+    init_dataset = init_dataset["train"].train_test_split(
+        train_size=0.5, test_size=0.5, stratify_by_column="label"
+    ).filter(
+        lambda x: x["label"]
+        != (
+            init_dataset["train"].features["label"].names.index("_unknown_")
+            and init_dataset["train"].features["label"].names.index("_silence_")
+        )
+    )
 
-model.fit(
-    train_x,
-    train["label"],
-    validation_data=(test_x, test["label"]),
-    batch_size=BATCH_SIZE,
-    epochs=MAX_EPOCHS,
-)
+    # batch training/testing data
+    init_dataset["train"] = init_dataset["train"].select(
+        [i for i in range((len(init_dataset["train"]) // BATCH) * BATCH)]
+    )
+    init_dataset["test"] = init_dataset["test"].select(
+        [i for i in range((len(init_dataset["test"]) // BATCH) * BATCH)]
+    )
+    print("dataset:\n", init_dataset)
+    labels = init_dataset["train"].features["label"].names
 
-preds = model.predict(test_x)
+    # pre-process our init_dataset dataset, removed unused columns
+    processed_init_dataset = init_dataset.map(
+        preprocess_function, remove_columns=["audio", "file"], batched=True
+    )
 
-rand_int = random.randint(0, len(test_x))
+    # load dataset as dict numpy arrays
+    train = processed_init_dataset["train"].shuffle(seed=42).with_format("numpy")[:]
+    test = processed_init_dataset["test"].shuffle(seed=42).with_format("numpy")[:]
 
-ipd.Audio(data=np.asarray(test_x["input_values"][rand_int]), autoplay=True, rate=16000)
+    model = build_model()
 
-model.save_model("./text-to-speech-TFWav2Vec2Model")
+    train_x = {x: y for x, y in train.items() if x != "label"}
+    test_x = {x: y for x, y in test.items() if x != "label"}
+
+    model.fit(
+        train_x,
+        train["label"],
+        validation_data=(test_x, test["label"]),
+        batch_size=BATCH,
+        epochs=1,
+    )
+    model.save_model("./text-to-speech-TFWav2Vec2Model")
+
+if __name__ == "__main__":
+    main()
